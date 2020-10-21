01a757192ceb6cf3ac9331f3e93c965216420bb4
sam <sam.leider@hotmail.com>
2020-10-21 18:49:22 -0400

    complete

---
+++ main.js
@@ -0,0 +1,27 @@
+var form = document.getElementsByTagName('form')[0];
+var inputButton = document.getElementsByTagName('input')[0];
+var outputDiv = document.querySelector('div.output-result');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    calculateFib(Number(inputButton.value));
+    inputButton.value = '';
+});
+var calculateFib = function (inputNumber) {
+    var a = 0;
+    var b = 1;
+    var results = [];
+    var count = 0;
+    while (count < inputNumber) {
+        results.push(a);
+        results.push(b);
+        a = a + b;
+        b = a + b;
+        count += 2;
+    }
+    var output = '';
+    for (var i = 0; i < results.length; i++) {
+        var element = results[i];
+        output += "\n      <li class=\"list-item\">\n        <strong>Step " + i + ":</strong> " + results[i] + "\n      </li>\n    ";
+    }
+    outputDiv.innerHTML = "\n    <ul class=\"output-list\">\n      " + output + "\n    </ul>\n  ";
+};
---
+++ main.ts
@@ -0,0 +1,42 @@
+const form:HTMLFormElement = document.getElementsByTagName('form')[0]
+const inputButton:HTMLInputElement = document.getElementsByTagName('input')[0]
+const outputDiv: HTMLDivElement = document.querySelector('div.output-result')
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  calculateFib(Number(inputButton.value))
+  inputButton.value = ''
+})
+
+const calculateFib = (inputNumber: number): void => {
+  let a = 0;
+  let b = 1;
+
+  const results: number[] = [];
+
+  let count:number = 0;
+  while(count < inputNumber) {
+    results.push(a)
+    results.push(b)
+    a = a + b;
+    b = a + b;
+    count+=2;
+  }
+  let output = ''
+  for (let i = 0; i < results.length; i++) {
+    const element = results[i];
+    output += `
+      <li class="list-item">
+        <strong>Step ${i}:</strong> ${results[i]}
+      </li>
+    `
+  }
+
+  outputDiv.innerHTML = `
+    <ul class="output-list">
+      ${output}
+    </ul>
+  `
+}
+
